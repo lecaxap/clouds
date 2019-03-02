02f059eea96562c0e65112678d28ab0b0b0c524d
Alisa <19alisapopova@gmail.com>
2019-03-02 11:59:40 +0200

    Alisa's commit 2

--- CollisionSystem.java
+++ CollisionSystem.java
@@ -25,27 +25,6 @@ public class CollisionSystem {
         pq.insert(new Event(t + 2, null, null));
     }
 
-    public void simulate(){
-        pq = new MinPQ<Event>();
-        for(int i = 0; i < N; i++) predict(particles[i]);
-        pq.insert(new Event(0, null, null));
-        while(!pq.isEmpty()){
-            Event event = pq.delMin();
-            if(!event.isValid()) continue;
-            Particle a = event.a;
-            Particle b = event.b;
-            for(int i = 0; i < N; i++)
-                particles[i].move(event.time - t);
-            t = event.time;
-            if (a != null && b != null) a.bounceOff(b);
-            else if (a != null && b == null) a.bounceOffVerticalWall();
-            else if (a == null && b != null) b.bounceOffHorizontalWall();
-            else if (a == null && b == null) redraw();
-            predict(a);
-            predict(b);
-        }
-    }
-
 
     private class Event implements Comparable<Event> {
         private double time; // time of event
